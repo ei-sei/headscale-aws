71f081588a7865c95be34bb4a7aa20bf55c70ea8
ei-sei <sk.ahm2019@gmail.com>
2026-05-27 22:17:37 +0100

    feat: add vpc configuration with nat gateway #3

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,15 @@ variable "environment" {
   type        = string
   default     = "dev"
 }
+
+variable "availability_zones" {
+  description = "List of availability zones to deploy resources in."
+  type        = list(string)
+  default     = ["eu-west-2a", "eu-west-2b"]
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/16"
+}
